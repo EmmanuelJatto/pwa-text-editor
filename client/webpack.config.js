7b31bc56ebd4949c1e98afb7fb27cb09ada160e3
Emmanuel Jatto <emmanueljatto33@gmail.com>
2024-09-12 13:57:40 -0400

    fixed issues in js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -32,7 +32,7 @@ module.exports = () => {
         fingerprints: false,
         inject: true,
         name: 'Just Another Text Editor',
-        short_name: 'J.A.T.E',
+        short_name: 'JATE',
         description: 'Takes notes with JavaScript syntax highlighting!',
         background_color: '#225ca3',
         theme_color: '#225ca3',
